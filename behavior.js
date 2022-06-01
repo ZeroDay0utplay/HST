850606ca47adf164b0b4a4d4913e0d7fe3b326f6
ZeroDayOutplay <hama.tm662@gmail.com>
2022-06-01 21:17:24 +0100

    finish JS

--- behavior.js
+++ behavior.js
@@ -1,1 +1,11 @@
-document.addEventListener()
+var me = document.getElementById("med");
+
+me.addEventListener("click", (event) => {
+    var classL = me.classList;
+    var is_small_there = false;
+    for (var i=0; i<classL.length; i++){
+        if (classL[i] === "small") is_small_there = true;
+    }
+    if (is_small_there) classL.remove("small");
+    else classL.add("small");
+});
